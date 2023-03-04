345fe6829a8fde3473061d222f679c809facf390
Ilariiiiia <nicolamigone179@gmail.com>
2023-03-04 13:42:16 +0000

    fixed build error

--- pages/_post.tsx
+++ pages/_post.tsx
@@ -1,4 +1,9 @@
-export default function Post({ title, content }){
+type PostProps = {
+	title:string;
+	content:string;
+}
+
+export default function Post({ title , content } : PostProps){
 	return <>
 	<div className="postWrapper">
 		<h3 className="postTitle">
